ff2e3c4edc69184e3bbed85794fc628917f4442b
Tiffani Hess <hesstjune@gmail.com>
2020-06-22 20:27:49 -0500

    connected to mongodb via mongoose and updated so that posts store in and render from db, ui changes to text and layout

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require("lodash");
+
 const port = process.env.PORT || 3000;
-
 const app = express();
-app.set("view engine", "ejs");
 
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 const posts = [];
-let dateSearch = "";
+
+// Database Setup
+mongoose.connect("mongodb://localhost:27017/healthDB", {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  rating: Number,
+  today: String,
+  time: String,
+  date: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 //Home page render
 app.get("/", (req, res) => {
-    res.render("home", {posts});
+    Post.find({}, (err, posts) => {
+        if (err) throw err;
+        res.render("home", {posts});
+    });
 });
 
 //Compose page render
@@ -26,11 +45,13 @@ app.get("/compose", (req,res) => {
 
 
 //Posts route
-app.get("/posts/:postQuery", (req,res) => {
-    const requestedPost = _.lowerCase(req.params.postQuery);
-    posts.forEach(post => {
+app.get("/posts/:postId", (req,res) => {
+    const postId = req.params.postId;
+    Post.findOne({_id: postId}, (err, post) => {
+        if(err) throw err;
+
         const date = post.date;
-        if(requestedPost === _.lowerCase(post.title) || requestedPost == date) {
+        
             res.render("posts", {
                 title: post.title,
                 content: post.content,
@@ -38,27 +59,36 @@ app.get("/posts/:postQuery", (req,res) => {
                 today: post.today,
                 time: post.time
             });
-        }
+        
+
     });
 });
+
 
 
 //Stat page render
 app.get("/stats", (req, res) => {
     let ratings = null;
     
-	posts.forEach((post) => {
-		ratings += Number(post.rating);
-    });
-    
-	const average = (ratings / posts.length).toFixed(1);
+	Post.find({}, (err, posts) => {
+        if (err) throw err;
 
-	res.render("stats", { average });
+        posts.forEach(post => {
+            // Grab all post ratings
+            ratings += Number(post.rating);
+        });
+       
+        // Find average of ratings
+        const average = (ratings / posts.length).toFixed(1);
+
+        res.render("stats", { average });
+    });
+		
 });
 
 app.get("/notFound", (req,res) => {
     res.render("notFound");
-})
+});
 
 //Compose page POST
 app.post("/compose", (req,res) => {
@@ -84,17 +114,17 @@ const options = { hour: "numeric", minute: "2-digit" };
     const content = req.body.postText;
     const rating = req.body.rating;
 
-    const post = {
+    const newPost = new Post({
         title: title,
         content: content,
         rating: rating,
         today: today,
         time: time,
         date: dateFormat
-    };
+    });
 
     
-    posts.push(post);
+    newPost.save();
     res.redirect("/");
 });
 
@@ -103,17 +133,23 @@ const options = { hour: "numeric", minute: "2-digit" };
 // TODO: Average over a week, month, etc.
 //TODO: Display list of posts if multiple match date search
 app.post("/stats", (req,res) =>{
-    dateSearch = req.body.dateSearch;
-    posts.forEach((post) => {
-    // If date of post and searched date match, open that post
-        if(post.date === dateSearch) {
-            const title = _.lowerCase(post.title);
-            res.redirect(`/posts/${title}`);
-        } else {
-            res.redirect("/notFound");
-        }
+    const dateSearch = req.body.dateSearch;
+    Post.findOne({ date: dateSearch}, (err, post) => {
+           if(!err) {
+            const id = post._id;
+            res.redirect(`/posts/${id}`);
+           } else {
+               throw err;
+               res.redirect("/notFound");
+           }
     });
+
+    // If date of post and searched date match, open that post
+     
+    
+    
 });
+
 
 
 app.listen(port, () => {
